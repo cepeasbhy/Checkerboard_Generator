943e3c4dc8a9741a3654daf129def049f011d607
cepeasbhy <asbhycepe01@gmail.com>
2020-12-04 19:46:40 +0800

    User input creation and input error handling

--- checkerboard.java
+++ checkerboard.java
@@ -1,5 +1,22 @@
 package checkerboard;
 
-public class checkerboard {
+import java.util.Scanner;
 
+public class checkerboard {
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		while(true) {
+			System.out.print("Enter a value for N: ");
+			int num = input.nextInt();
+			
+			if(num <= 0) {
+				System.out.println("Invalid Input!");
+			}else {
+				break;
+			}
+		}
+	}
+	
 }
